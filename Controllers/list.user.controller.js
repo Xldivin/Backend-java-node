cbf297078984aa63fcf98389593d676a1af432d2
shyaka axel divin <Xldivin@gmail.com>
2022-12-13 13:23:01 +0200

    fx(Node)
    Node middleware java
    
    finish main

--- Controllers/list.user.controller.js
+++ Controllers/list.user.controller.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const User = (req, res) => {
     let url = "http://localhost:8080/MedicalRecords2/Users"
     fetch(url).then((response)=>{
-        response.JSON.stringify().then((res1)=>{
+        response.json().then((res1)=>{
                 return res.status(200).send({
                     success: true,
                     data:res1,
