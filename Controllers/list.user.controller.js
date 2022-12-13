79dc21d231ea1290a5393de8817d3155812b23ab
shyaka axel divin <Xldivin@gmail.com>
2022-12-13 07:52:01 +0200

    new changes

--- Controllers/list.user.controller.js
+++ Controllers/list.user.controller.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const User = (req, res) => {
     let url = "http://localhost:8080/MedicalRecords2/Users"
     fetch(url).then((response)=>{
-        response.text().then((res1)=>{
+        response.JSON.stringify().then((res1)=>{
                 return res.status(200).send({
                     success: true,
                     data:res1,
